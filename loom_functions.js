c22b82cada1e652d97f938c5fbe7a8908c21d49c
ACatThatPrograms <acatthatprograms@gmail.com>
2018-07-25 15:33:17 -0400

    Export Test Functions to loom_functions

--- loom_functions.js
+++ loom_functions.js
@@ -1,30 +1,78 @@
 var exports = module.exports = {};
 
-const {
-  NonceTxMiddleware, SignedTxMiddleware, Client,
-  Contract, Address, LocalAddress, CryptoUtils
-} = require('loom-js')
-
 exports.message = function message(msg){
   console.log(msg);
 }
 
-exports.getContract = async function getContract(privateKey, publicKey) {
+const {
+  NonceTxMiddleware, SignedTxMiddleware, Client, ClientEvent,
+  Contract, Address, LocalAddress, CryptoUtils
+} = require('loom-js')
+
+const { MapEntry } = require('./helloworld_pb')
+
+/**
+ * Creates a new `Contract` instance that can be used to interact with a smart contract.
+ * @param privateKey Private key that will be used to sign transactions sent to the contract.
+ * @param publicKey Public key that corresponds to the private key.
+ * @returns `Contract` instance.
+ */
+async function getContract(privateKey, publicKey) {
   const client = new Client(
     'default',
-    'ws://192.168.1.220:46657/websocket',
-    'ws://192.168.1.220:9999/queryws'
+    'ws://127.0.0.1:46657/websocket',
+    'ws://127.0.0.1:9999/queryws'
   )
+
+  client.on(ClientEvent.Error, err => console.log(err || 'Unexpected Client Error'))
+
   // required middleware
   client.txMiddleware = [
     new NonceTxMiddleware(publicKey, client),
     new SignedTxMiddleware(privateKey)
   ]
+
   const contractAddr = await client.getContractAddressAsync('BluePrint')
+    if (!contractAddr) {
+      throw new Error('Failed to resolve contract address')
+  }
   const callerAddr = new Address(client.chainId, LocalAddress.fromPublicKey(publicKey))
   return new Contract({
     contractAddr,
     callerAddr,
     client
   })
+}
+
+/**
+ * Stores an association between a key and a value in a smart contract.
+ * @param contract Contract instance returned from `getContract()`.
+ */
+async function store(contract, key, value) {
+  const params = new MapEntry()
+  params.setKey(key)
+  params.setValue(value)
+  await contract.callAsync('SetMsg', params)
+}
+
+/**
+ * Loads the value associated with a key in a smart contract.
+ * @param contract Contract instance returned from `getContract()`.
+ */
+async function load(contract, key) {
+  const params = new MapEntry()
+  // The smart contract will look up the value stored under this key.
+  params.setKey(key)
+  const result = await contract.staticCallAsync('GetMsg', params, new MapEntry())
+  return result.getValue()
+}
+
+exports.blueprintTest = async function blueprintTest() {
+  const privateKey = CryptoUtils.generatePrivateKey()
+  const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey)
+
+  const contract = await getContract(privateKey, publicKey)
+  await store(contract, '123', 'hello!')
+  const value = await load(contract, '123')
+  console.log('Value: ' + value)
 }
